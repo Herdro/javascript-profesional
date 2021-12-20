706b22578bb375967e3e0ae11e3fc2ab5df1a2bd
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-12-20 17:38:59 -0300

    implementar play/pause

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,18 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
 function MediaPlayer(config) {
   this.media = config.el;
 }
 
 MediaPlayer.prototype.play = function() {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
   if (this.media.paused) {
-    this.play();
+    this.media.play();
   } else {
-    this.pause();
+    this.media.pause();
   }
 };
 
-const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video});
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+button.onclick = () => player.play();
