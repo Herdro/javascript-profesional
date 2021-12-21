23cd20e26415501960283bf91632b65103a3cd03
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-12-21 16:07:37 -0300

    reto button muted and unmuted

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,13 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [
+    new AutoPlay()
+] });
 
-const button = document.querySelector('button');
+const button = document.querySelector('button.Play');
 button.onclick = () => player.togglePlay();
+
+const buttonMute = document.querySelector('button.Mute');
+buttonMute.onclick = () => player.toggleMute();
